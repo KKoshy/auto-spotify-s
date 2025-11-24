efb0ca2d2d08b01e00aef0e6ae6e09340d761e41
KKoshy <40890763+KKoshy@users.noreply.github.com>
2025-11-24 19:53:08 +0530

    Delete libs/ui/locators.py

--- libs/ui/locators.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class HomePageSelectors:
-    HOME_BUTTON = (By.XPATH, "//button[@data-testid='home-button']")
-    SEARCH_BUTTON = (By.XPATH, "//button[@aria-label='Search']")
-    SEARCH_INPUT = (By.XPATH, "//input[@data-testid='search-input']")
-    SEARCH_CLEAR_BUTTON = (By.XPATH, "//button[@data-testid='clear-button']")
-    BROWSE_BUTTON = (By.XPATH, "//button[@data-testid='browse-button']")
