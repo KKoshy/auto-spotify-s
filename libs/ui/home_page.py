b858a577e56f7b5c412ae82feb23a9cff396851d
KKoshy <40890763+KKoshy@users.noreply.github.com>
2025-11-24 19:52:56 +0530

    Delete libs/ui/home_page.py

--- libs/ui/home_page.py
+++
@@ -1,9 +0,0 @@
-from libs.ui.base_page import BasePage
-
-class HomePage(BasePage):
-    def __init__(self, driver):
-        self.url = "https://open.spotify.com"
-        self.driver = driver
-        super().__init__(driver)
-
-    
