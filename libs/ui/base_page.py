e7d90e3ae9b3948ae57ea2427308413fa1034b7c
KKoshy <40890763+KKoshy@users.noreply.github.com>
2025-11-24 21:37:13 +0530

    Implement screenshot saving in BasePage
    Added a method to save screenshots with allure attachment.

--- libs/ui/base_page.py
+++ libs/ui/base_page.py
@@ -1,3 +1,6 @@
+import os
+import allure
+from uuid import uuid4
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -20,3 +23,8 @@ class BasePage:
 
     def get_displayed_state(self, locator):
         return self.find(locator).is_displayed()
+    
+    def save_screenshot(self):
+        img_path = os.path.join("allure-report", "{}.png".format(uuid4()))
+        self.driver.save_screenshot(img_path)
+        allure.attach.file(source=img_path)
