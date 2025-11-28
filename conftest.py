97508db0ce5429339e9695b28f79fdaea4d06c7a
KKoshy <40890763+KKoshy@users.noreply.github.com>
2025-11-28 18:14:40 +0530

    Configure ChromeDriver options for tests

--- conftest.py
+++ conftest.py
@@ -12,6 +12,15 @@ log = logging.getLogger(__name__)
 @pytest.fixture(scope='module')
 def chrome_driver():
     options = Options() 
+    # options = webdriver.ChromeOptions()
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    options.headless = False
     driver = Chrome(options=options)
     driver.get("https://open.spotify.com")
     yield driver
