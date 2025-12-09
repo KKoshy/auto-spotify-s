83ed8faebea3f71e139e044916e1743ddd77e277
KKoshy <40890763+KKoshy@users.noreply.github.com>
2025-12-09 17:52:43 +0530

    Use secrets for random string generation
    Replaced random string generation with secrets for better security.

--- libs/ui/song_data.py
+++ libs/ui/song_data.py
@@ -1,9 +1,9 @@
 import string
-import random
+import secrets
 from dataclasses import dataclass, field
 
 @dataclass
 class PlaylistData:
-    rand: str = "".join(random.choices(string.ascii_lowercase, k=7))
+    rand: str = "".join(secrets.choice(string.ascii_lowercase) for _ in range(7))
     name: str = field(init=False, default=f"{rand} playlist")
     description: str = field(init=False, default=f"{rand} playlist description")
