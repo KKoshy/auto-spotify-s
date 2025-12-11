0a58fd1f3c0abe80382e43e3e7efcfeb8d35476d
KKoshy <40890763+KKoshy@users.noreply.github.com>
2025-12-11 08:41:25 +0530

    Update song_data.py

--- libs/ui/song_data.py
+++ libs/ui/song_data.py
@@ -6,4 +6,5 @@ from dataclasses import dataclass, field
 class PlaylistData:
     rand: str = "".join(secrets.choice(string.ascii_lowercase) for _ in range(7))
     name: str = field(init=False, default=f"{rand} playlist")
+    pl_id: str = field(init=False, default="")
     description: str = field(init=False, default=f"{rand} playlist description")
