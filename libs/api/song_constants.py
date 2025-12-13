cbc0c279d4f24feff7a4553616eed73e07cdb9b6
KKoshy <40890763+KKoshy@users.noreply.github.com>
2025-12-13 19:55:53 +0530

    Create song_constants.py

---
+++ libs/api/song_constants.py
@@ -0,0 +1,7 @@
+class ArtistAPIConstants:
+    artist_id = "0TnOYISbd1XYRBk9myaseg"
+    json_content = "application/json; charset=utf-8"
+    cache_control = "public, max-age=0"
+    retry_after_header = "Retry-After"
+    artist_name = "Pitbull"
+    artist_uri = "spotify:artist:0TnOYISbd1XYRBk9myaseg"
