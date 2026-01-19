c2b689b703db27c6466a0e9e7f3c0fbd3c90e03a
KKoshy <40890763+KKoshy@users.noreply.github.com>
2026-01-19 21:01:52 +0530

    Update login_page.py

--- libs/ui/login_page.py
+++ libs/ui/login_page.py
@@ -44,6 +44,52 @@ class LoginPage(BasePage):
         self.click(LoginPageSelectors.CONTINUE_LOGIN_BUTTON)
         self.save_screenshot()
         return self
+    
+    def should_have_username_error(self) -> "LoginPage":
+        """
+        Validating presence of username error
+
+        :return: self for method chaining
+        """
+        log.info("Validating presence of username error")
+        by, xpath = LoginPageSelectors.USER_ERROR
+        assert self.get_displayed_state((by, xpath.format(msg="Please enter your Spotify username or email address.")))
+        self.save_screenshot()
+        return self
+    
+    def should_have_no_account_error(self) -> "LoginPage":
+        """
+        Validating presence of no account error
+
+        :return: self for method chaining
+        """
+        log.info("Validating presence of user with no account error")
+        by, xpath = LoginPageSelectors.USER_ERROR
+        assert self.get_displayed_state((by, xpath.format(msg="Email or username isn't linked to a Spotify account")))
+        self.save_screenshot()
+        return self
+    
+    def should_have_no_password_error(self) -> "LoginPage":
+        """
+        Validating presence of password error
+
+        :return: self for method chaining
+        """
+        log.info("Validating presence of password error")
+        assert self.get_displayed_state(LoginPageSelectors.NO_PASSWORD_ERROR)
+        self.save_screenshot()
+        return self
+        
+    def should_have_incorrect_creds_error(self) -> "LoginPage":
+        """
+        Validating presence of incorrect username/password error
+
+        :return: self for method chaining
+        """
+        log.info("Validating presence of incorrect username/password error")
+        assert self.get_displayed_state(LoginPageSelectors.INCORRECT_CREDS)
+        self.save_screenshot()
+        return self
 
     @retry(
         stop=stop_after_attempt(5),
@@ -58,4 +104,3 @@ class LoginPage(BasePage):
         if self.get_displayed_state(LoginPageSelectors.LOGIN_ALERT):
             self.save_screenshot()
             raise SystemError("Unexpected error")
-
