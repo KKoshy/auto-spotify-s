7bf44c93e0662098376b193be4953f1623806ec6
KKoshy <40890763+KKoshy@users.noreply.github.com>
2026-01-27 20:18:38 +0530

    Add PlaylistAPIConstants class for API payloads
    Define constants for Playlist API interactions including payloads for adding and removing items.

---
+++ libs/api/playlists/playlist_constants.py
@@ -0,0 +1,26 @@
+class PlaylistAPIConstants:
+    nightly_playlist_id = "1Ju3Rs186TgQuou1JCZbjz"
+    follow_playlist_payload = {
+        "public": True
+    }
+    add_items_payload = {
+        "uris": [
+            # believer
+            "spotify:track:0pqnGHJpmpxLKifKRmU6WP",
+            # blue
+            "spotify:track:3be9ACTxtcL6Zm4vJRUiPG",
+            # die with a smile
+            "spotify:track:2plbrEY59IikOBgBGLjaoe",
+        ],
+        "position": 0
+    }
+    remove_items_payload = {
+        "tracks": [
+            {
+                "uri": "spotify:track:0pqnGHJpmpxLKifKRmU6WP",
+                "uri": "spotify:track:3be9ACTxtcL6Zm4vJRUiPG",
+                "uri": "spotify:track:2plbrEY59IikOBgBGLjaoe"
+            }
+        ],
+        "snapshot_id": "string"
+    }
