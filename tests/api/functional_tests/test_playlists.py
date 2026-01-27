823937c6880fe800312d453b2e0b2bf4257975e7
KKoshy <40890763+KKoshy@users.noreply.github.com>
2026-01-27 20:17:41 +0530

    Add functional tests for Playlists API
    Implement functional tests for Playlists API including follow, add, remove, and unfollow operations.

---
+++ tests/api/functional_tests/test_playlists.py
@@ -0,0 +1,51 @@
+import logging
+import pytest
+from libs.api.playlists.playlists_api import PlaylistsAPI
+from libs.api.playlists.playlist_constants import PlaylistAPIConstants
+from libs.api.users.users_api import UsersAPI
+
+log = logging.getLogger(__name__)
+
+SNAPSHOT_ID = None
+
+# TODO: data validation
+
+@pytest.mark.positive
+class TestPlaylistsAPI:
+    @pytest.mark.dependency(name='follow')
+    def test_follow_playlist(self, playlists: PlaylistsAPI, users: UsersAPI):
+        log.info("Verifying follow playlist")
+        follow = users.follow_playlist(playlist_id=PlaylistAPIConstants.nightly_playlist_id, 
+                                       json=PlaylistAPIConstants.follow_playlist_payload)
+        assert follow.status_code == 200
+        user_playlists = playlists.get_current_user_playlists()
+        assert user_playlists.status_code == 200
+
+    @pytest.mark.dependency(name='add_items', depends=['follow'])
+    def test_add_playlist_items(self, playlists: PlaylistsAPI):
+        log.info("Verifying adding items to playlist")
+        add_items = playlists.add_playlist_items(playlist_id=PlaylistAPIConstants.nightly_playlist_id, 
+                                                 json=PlaylistAPIConstants.add_items_payload)
+        assert add_items.status_code == 201
+        global SNAPSHOT_ID
+        SNAPSHOT_ID = add_items.json().get('snapshot_id')
+        current_items = playlists.get_playlist_items(playlist_id=PlaylistAPIConstants.nightly_playlist_id)
+        assert current_items.status_code == 200
+
+    @pytest.mark.dependency(depends=['add_items'])
+    def test_remove_playlist_items(self, playlists: PlaylistsAPI):
+        log.info("Verifying removal of items from playlist")
+        PlaylistAPIConstants.remove_items_payload["snapshot_id"] = SNAPSHOT_ID
+        remove_items = playlists.remove_playlist_items(playlist_id=PlaylistAPIConstants.nightly_playlist_id,
+                                                       json=PlaylistAPIConstants.remove_items_payload)
+        assert remove_items.status_code == 200
+        current_items = playlists.get_playlist_items(playlist_id=PlaylistAPIConstants.nightly_playlist_id)
+        assert current_items.status_code == 200
+
+    @pytest.mark.dependency(depends=['follow'])
+    def test_unfollow_playlist(self, playlists: PlaylistsAPI, users: UsersAPI):
+        log.info("Verifying unfollow playlist")
+        unfollow = users.unfollow_playlist(playlist_id=PlaylistAPIConstants.nightly_playlist_id)
+        assert unfollow.status_code == 200
+        user_playlists = playlists.get_current_user_playlists()
+        assert user_playlists.status_code == 200
