c6ec6f41517a5b90d135a067a165d6184ec8d830
KKoshy <40890763+KKoshy@users.noreply.github.com>
2026-01-29 19:51:16 +0530

    Refactor artist tests for improved validation
    Updated artist fixture to include status code and JSON schema validation. Removed redundant tests and adjusted negative test cases to use ResponseConstants.

--- tests/api/unit_tests/test_artist.py
+++ tests/api/unit_tests/test_artist.py
@@ -1,23 +1,19 @@
-import os
-import json
 import logging
 import pytest
-from jsonschema import validate
+from data.api.response_constants import ResponseConstants
 from libs.api.artists.song_constants import ArtistAPIConstants
 
 log = logging.getLogger(__name__)
 
 @pytest.fixture(scope='class')
 def artist(artists):
-    artist = artists.get_artist(artist_id=ArtistAPIConstants.artist_id)
+    artist = artists.get_artist(artist_id=ArtistAPIConstants.artist_id,
+                                status_code=ResponseConstants.status_200,
+                                json_schema=ArtistAPIConstants.artist_schema)
     return artist
 
 @pytest.mark.positive
 class TestArtistAPI:
-    def test_response_code(self, artist):
-        log.info("Verifying response code")
-        assert artist.status_code == 200
-
     def test_content_type_header(self, artist):
         log.info("Verifying content type")
         assert artist.headers.get('content-type') == ArtistAPIConstants.json_content
@@ -29,11 +25,6 @@ class TestArtistAPI:
     def test_retry_after_header(self, artist):
         log.info("Verifying absence of retry after header")
         assert not artist.headers.get(ArtistAPIConstants.retry_after_header)
-
-    def test_json_schema(self, artist):
-        log.info("Verifying JSON schema")
-        validate(instance=artist.json(), 
-                 schema=json.load(open(os.path.join("data", "json_schema", "artist_schema.json"))))
         
     def test_fields(self, artist):
         log.info("Validate data fields")
@@ -46,10 +37,10 @@ class TestArtistAPI:
 @pytest.mark.negative
 class TestNegativeArtistAPI:
     @pytest.mark.parametrize("artist_id,status_code,token,message",
-                             [("", 400, None, 'Missing required field: ids'),
-                              (None, 400, None, 'Invalid base62 id'),
-                              (ArtistAPIConstants.artist_id[:-3] + 'abc', 404, None, 'Resource not found'),
-                              (ArtistAPIConstants.artist_id, 401, "asdoinsadnfasoidnsione", 'Invalid access token'),],
+                             [("", ResponseConstants.status_400, None, 'Missing required field: ids'),
+                              (None, ResponseConstants.status_400, None, 'Invalid base62 id'),
+                              (ArtistAPIConstants.artist_id[:-3] + 'abc', ResponseConstants.status_404, None, 'Resource not found'),
+                              (ArtistAPIConstants.artist_id, ResponseConstants.status_401, "asdoinsadnfasoidnsione", 'Invalid access token'),],
                               ids=["missing_artist_id", "invalid_id", "incorrect_id", "invalid_token"])
     def test_negative_case(self, artists, artist_id, token, status_code,message):
         log.info("Verifying negative case of Artist API")
