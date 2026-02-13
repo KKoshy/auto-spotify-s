ec1fc9c4be8c689aa24579a5ec74f62ac4e4c1e0
KKoshy <40890763+KKoshy@users.noreply.github.com>
2026-02-13 12:32:31 +0530

    Add docstring for API response constants

--- data/api/response_constants.py
+++ data/api/response_constants.py
@@ -1,7 +1,10 @@
+"""
+This page holds API response constants
+"""
+
 class ResponseConstants:
     status_200 = 200
     status_201 = 201
     status_400 = 400
     status_401 = 401
     status_404 = 404
-    
