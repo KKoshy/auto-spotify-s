8f9ba4e2bdf8d182a331d0e5929625f914408dbf
KKoshy <40890763+KKoshy@users.noreply.github.com>
2026-02-13 18:13:55 +0530

    Update left_sidebar_page.py

--- libs/ui/left_sidebar_page.py
+++ libs/ui/left_sidebar_page.py
@@ -4,12 +4,15 @@ POM for Left side bar
 
 import logging
 import re
+
+from selenium.webdriver.support import expected_conditions as EC
+
 from libs.ui.base_page import BasePage
 from libs.ui.locators import LeftSideBarSelectors
 from libs.ui.playlist_details_page import PlaylistDetailsPage
-from selenium.webdriver.support import expected_conditions as EC
 
 log = logging.getLogger(__name__)
+
 
 class LeftSideBarPage(BasePage):
     def __init__(self, driver):
@@ -25,15 +28,15 @@ class LeftSideBarPage(BasePage):
         log.info("Creating playlist")
         self.click(LeftSideBarSelectors.CREATE_PL_FL)
         self.click(LeftSideBarSelectors.CREATE_PLAYLIST_OPTION)
-        pattern = rf'{self.base_url}/playlist/([a-zA-Z0-9]+)'
+        pattern = rf"{self.base_url}/playlist/([a-zA-Z0-9]+)"
         self.wait.until(EC.url_matches(pattern))
         pl_id = re.findall(pattern, self.driver.current_url)[0]
         return PlaylistDetailsPage(self.driver, pl_id)
-    
+
     def open_library(self) -> "LeftSideBarPage":
         """
         Opening the library
-        
+
         :return: current instance of LeftSideBarPage
         """
         log.info("Open library")
@@ -41,11 +44,11 @@ class LeftSideBarPage(BasePage):
             self.click(LeftSideBarSelectors.OPEN_LIBRARY)
             assert self.get_enabled_state(LeftSideBarSelectors.COLLAPSE_LIBRARY)
         return self
-    
+
     def search_playlist(self, playlist: str) -> "LeftSideBarPage":
         """
         Search for the playlist in library
-        
+
         :param playlist: playlist name
         :return: current instance of LeftSideBarPage
         """
@@ -54,11 +57,11 @@ class LeftSideBarPage(BasePage):
             self.click(LeftSideBarSelectors.SEARCH_BUTTON)
         self.type(LeftSideBarSelectors.SEARCH_BAR, playlist)
         return self
-    
+
     def should_have_playlist(self, playlist: str) -> "LeftSideBarPage":
         """
         Validating the presence of playlist in the library
-        
+
         :param playlist: playlist name
         :return: current instance of LeftSideBarPage
         """
@@ -67,17 +70,18 @@ class LeftSideBarPage(BasePage):
         by, xpath = LeftSideBarSelectors.PLAYLIST_TEMPLATE
         assert self.get_displayed_state((by, xpath.format(playlist=playlist)))
         return self
-    
+
     def should_not_have_playlist(self, playlist: str) -> "LeftSideBarPage":
         """
         Validating the absence of playlist in the library
-        
+
         :param playlist: playlist name
         :return: current instance of LeftSideBarPage
         """
         log.info(f"Validating the absence of playlist {playlist}")
         self.search_playlist(playlist=playlist)
         by, xpath = LeftSideBarSelectors.PLAYLIST_TEMPLATE
-        assert not self.get_displayed_state((by, xpath.format(playlist=playlist)))
+        assert not self.get_displayed_state(
+            (by, xpath.format(playlist=playlist))
+        )
         return self
-
